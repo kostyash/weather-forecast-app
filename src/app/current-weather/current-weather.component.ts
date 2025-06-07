3630298ef8a19d470d8837ab564ba09b8762668d
kostyash <kostyash11@gmail.com>
2025-06-07 21:32:41 +0300

    Refactor current weather component to use optional chaining for weather data access and correct input property declaration.

--- src/app/current-weather/current-weather.component.ts
+++ src/app/current-weather/current-weather.component.ts
@@ -1,4 +1,4 @@
-import { ChangeDetectionStrategy, Component, Input } from '@angular/core';
+import { ChangeDetectionStrategy, Component, input } from '@angular/core';
 import { MatCardModule } from '@angular/material/card';
 import { CurrentWeather } from '../contracts';
 
@@ -7,9 +7,8 @@ import { CurrentWeather } from '../contracts';
   imports: [MatCardModule],
   templateUrl: './current-weather.component.html',
   styleUrl: './current-weather.component.scss',
-  changeDetection: ChangeDetectionStrategy.OnPush
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class CurrentWeatherComponent {
-
-  @Input() weather!: CurrentWeather;
+  weather = input<CurrentWeather>();
 }
